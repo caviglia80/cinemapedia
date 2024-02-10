ac6654b293785541b0f8030b7894a0fde289dd4c
Francisco <caviglia80@gmail.com>
2024-02-10 17:47:45 +0100

    .

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
@@ -8,10 +9,19 @@ class CustomBottomNavigation extends StatelessWidget {
     return BottomNavigationBar(
       elevation: 0,
       items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
-        BottomNavigationBarItem(icon: Icon(Icons.label_outline), label: 'Categorias'),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
-      ],
+        BottomNavigationBarItem(
+          icon: Icon( Icons.home_max ),
+          label: 'Inicio'
+        ),
+        BottomNavigationBarItem(
+          icon: Icon( Icons.label_outline ),
+          label: 'Categorías'
+        ),
+        BottomNavigationBarItem(
+          icon: Icon( Icons.favorite_outline ),
+          label: 'Favoritos'
+        ),
+      ]
     );
   }
 }
